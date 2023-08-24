4520e374def3c55fe27dd3f4b8876eb513e8b8cf
DaviAndreozzi <Daviandreozzi7@gmail.com>
2023-08-24 19:12:52 -0300

    lili

--- front/src/constants/BASE_URL.js
+++ front/src/constants/BASE_URL.js
@@ -1,3 +1,3 @@
-export const Base_URL_USERS = "https://projeto-final-rkig.onrender.com/users";
+export const Base_URL_USERS = "https://projeto-final-labbedit.onrender.com/users";
 
-export const Base_URL_POST = "https://projeto-final-rkig.onrender.com/postagens";
+export const Base_URL_POST = "https://projeto-final-labbedit.onrender.com/postagens";
